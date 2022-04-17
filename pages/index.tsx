0e3e986cfa88ca9c0c3d35f2c64767c888c9aab3
miruoon <rillakerika@gmail.com>
2022-04-17 23:14:33 +0900

    スキルカード作りはじめ

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,39 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
 
+const skills = [
+  {
+    name: 'JavaScript',
+    levelPercentage: 0.8,
+    experienced: true,
+  },
+  {
+    name: 'TypeScript',
+    levelPercentage: 0.6,
+    experienced: true,
+  },
+  {
+    name: 'Vue',
+    levelPercentage: 0.7,
+    experienced: true,
+  },
+  {
+    name: 'Nuxt.js',
+    levelPercentage: 0.6,
+    experienced: true,
+  },
+  {
+    name: 'React',
+    levelPercentage: 0.7,
+    experienced: false,
+  },
+  {
+    name: 'Next.js',
+    levelPercentage: 0.7,
+    experienced: false,
+  },
+]
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -13,21 +46,31 @@ const Home: NextPage = () => {
         <Image
           src="https://avatars.githubusercontent.com/u/42486288?s=400&u=567eea2c478e5baed1af37987097a8facd5ee0ca&v=4"
           alt="profile-image"
-          width={400}
-          height={400}
+          width={300}
+          height={300}
         />
         <div className={styles.card__profile}>
           <div className={styles.card__profile__name}>
             <h1>Erika Yoshikawa</h1>
-            <p>Front-end developer</p>
+            <p>Web developer</p>
           </div>
           <div>
-            {/* TODO: 修正予定 */}
-            Self-motivated developer, who is willing to learn and create
-            outstanding UI applications. Donec aliquam est dui, vel vestibulum
-            diam sollicitudin id. Quisque feugiat malesuada molestie.
+            Web歴約一年半のWebエンジニア。ヌオーの人。メインはフロントエンド屋だが、バックエンドも機会があれば経験を積みたい。
           </div>
         </div>
+      </div>
+      {/* 2段目のカード（ブログ/スキル） */}
+      <div className={styles.card__skills}>
+        <>
+          <div className={styles.card__skillHead}>Front end</div>
+          {skills.map((skill) => (
+            <div key={skill.name} className={styles.card__skill}>
+              <div>{skill.name}</div>
+              {/* スキルグラフ */}
+              <div></div>
+            </div>
+          ))}
+        </>
       </div>
 
       <footer className={styles.footer}>
